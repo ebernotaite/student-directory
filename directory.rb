0d8ad72c4c59877e7777f5ecdb508c476ef6bef2
Egle Bernotaite <ebernotaite94@gmail.com>
2019-05-10 10:47:32 +0100

    Adding methods

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-student_list = [
+students = [
   "Dr. Hannibal Lecter",
   "Darth Vader",
   "Nurse Ratched",
@@ -12,9 +12,21 @@ student_list = [
   "Norman Bates"
 ]
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header 
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-print "Overall, we have #{students.count} great students "
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students "
+end
+
+print_header
+print(students)
+print_footer(students)
